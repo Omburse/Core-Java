52ca075239d7a3ec5133266cd878b3ef88ca8fa3
OM BURSE <omburse0019@gmail.com>
2026-03-20 22:34:18 +0530

    Loops in java

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args){
+        int i = 0;
+        for(int counter = 0 ; counter<=10; counter++ ){
+            System.out.print(counter + " ");
+        }
+    }
+}
