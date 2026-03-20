5d51629fc0765c2f101ad5ae805ebeb19ef2c264
OM BURSE <omburse0019@gmail.com>
2026-03-20 21:08:32 +0530

    Operators In Java

---
+++ Operators.java
@@ -0,0 +1,38 @@
+public class Operators {
+    public static void main(String[]args){
+        // Arithmetic Operators
+        // Binary Operators
+        /*
+        int a = 15;
+        int b = 17;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+         */
+
+        // Unary Operators
+
+        /*
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+         */
+
+        // Relatioal Operators
+
+        /*
+        int a = 10;
+        int b = 5;
+        System.out.println("The equal are : " + (a==b));
+        System.out.println("The Not equal To are : " + (a!=b));
+        System.out.println("The A is grater than B are : " + (a>b));
+        System.out.println("The A is less than B are : " + (a<b));
+        System.out.println("The A is grater than or equal to are : " + (a>=b));
+        System.out.println("The A is less than or equal to are : " + (a<=b));
+         */
+    }
+}
